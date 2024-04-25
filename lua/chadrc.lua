0b86a38056d656c76af61f84a98758830bef6a2e
im <ilya.mikhailov4@gmail.com>
2024-04-25 22:58:20 +0300

    Plugins changes. Enabled dashboard on startup. Added LSP

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -7,6 +7,30 @@ local M = {}
 M.ui = {
 	theme = "catppuccin",
 
+    nvdash = {
+        load_on_startup = true,
+
+        header = {
+          "           ▄ ▄                   ",
+          "       ▄   ▄▄▄     ▄ ▄▄▄ ▄ ▄     ",
+          "       █ ▄ █▄█ ▄▄▄ █ █▄█ █ █     ",
+          "    ▄▄ █▄█▄▄▄█ █▄█▄█▄▄█▄▄█ █     ",
+          "  ▄ █▄▄█ ▄ ▄▄ ▄█ ▄▄▄▄▄▄▄▄▄▄▄▄▄▄  ",
+          "  █▄▄▄▄ ▄▄▄ █ ▄ ▄▄▄ ▄ ▄▄▄ ▄ ▄ █ ▄",
+          "▄ █ █▄█ █▄█ █ █ █▄█ █ █▄█ ▄▄▄ █ █",
+          "█▄█ ▄ █▄▄█▄▄█ █ ▄▄█ █ ▄ █ █▄█▄█ █",
+          "    █▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█ █▄█▄▄▄█    ",
+        },
+
+        buttons = {
+          { "  Find File", "Spc f f", "Telescope find_files" },
+          { "󰈚  Recent Files", "Spc f o", "Telescope oldfiles" },
+          { "󰈭  Find Word", "Spc f w", "Telescope live_grep" },
+          { "  Bookmarks", "Spc m a", "Telescope marks" },
+          { "  Themes", "Spc t h", "Telescope themes" },
+          { "  Mappings", "Spc c h", "NvCheatsheet" },
+        },
+      },
 	-- hl_override = {
 	-- 	Comment = { italic = true },
 	-- 	["@comment"] = { italic = true },
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,48 +1,55 @@
 return {
-    {
-        "stevearc/conform.nvim",
-        -- event = 'BufWritePre', -- uncomment for format on save
-        config = function()
-            require "configs.conform"
-        end,
+  {
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre', -- uncomment for format on save
+    config = function()
+      require "configs.conform"
+    end,
+  },
+  { "Pocco81/auto-save.nvim", opts = {}, lazy = false },
+  {
+    "christoomey/vim-tmux-navigator",
+    cmd = {
+      "TmuxNavigateLeft",
+      "TmuxNavigateDown",
+      "TmuxNavigateUp",
+      "TmuxNavigateRight",
+      "TmuxNavigatePrevious",
     },
-    { "Pocco81/auto-save.nvim", opts = {}, },
-    {
-        "christoomey/vim-tmux-navigator",
-        cmd = {
-            "TmuxNavigateLeft",
-            "TmuxNavigateDown",
-            "TmuxNavigateUp",
-            "TmuxNavigateRight",
-            "TmuxNavigatePrevious",
-        },
-    }
-    -- These are some examples, uncomment them if you want to see them work!
-    -- {
-    --   "neovim/nvim-lspconfig",
-    --   config = function()
-    --     require("nvchad.configs.lspconfig").defaults()
-    --     require "configs.lspconfig"
-    --   end,
-    -- },
-    --
-    -- {
-    -- 	"williamboman/mason.nvim",
-    -- 	opts = {
-    -- 		ensure_installed = {
-    -- 			"lua-language-server", "stylua",
-    -- 			"html-lsp", "css-lsp" , "prettier"
-    -- 		},
-    -- 	},
-    -- },
-    --
-    -- {
-    -- 	"nvim-treesitter/nvim-treesitter",
-    -- 	opts = {
-    -- 		ensure_installed = {
-    -- 			"vim", "lua", "vimdoc",
-    --      "html", "css"
-    -- 		},
-    -- 	},
-    -- },
+  },
+  -- These are some examples, uncomment them if you want to see them work!
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require("nvchad.configs.lspconfig").defaults()
+      require "configs.lspconfig"
+    end,
+  },
+
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "lua-language-server",
+        "stylua",
+        "html-lsp",
+        "css-lsp",
+        "prettier",
+        "pyright",
+      },
+    },
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "vim",
+        "lua",
+        "vimdoc",
+        "html",
+        "css",
+        "python",
+      },
+    },
+  },
 }
