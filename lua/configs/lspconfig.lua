94a69ed74d32658b3cffdf874567475a0a82d3dd
im <ilya.mikhailov4@gmail.com>
2024-12-28 19:32:15 +0200

    Added ansible. Format changes

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,33 +4,57 @@ local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "html", "cssls", "pyright", "clangd" }
+local servers = { "html", "cssls", "pyright", "clangd", "ansiblels" }
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
-    lspconfig[lsp].setup {
-        on_attach = on_attach,
-        on_init = on_init,
-        capabilities = capabilities,
-    }
+  lspconfig[lsp].setup {
+    on_attach = on_attach,
+    on_init = on_init,
+    capabilities = capabilities,
+  }
 end
 
 -- typescript
 lspconfig.ts_ls.setup {
-    on_attach = on_attach,
-    on_init = on_init,
-    capabilities = capabilities,
+  on_attach = on_attach,
+  on_init = on_init,
+  capabilities = capabilities,
 }
 
 lspconfig.pyright.setup {
-    on_attach = on_attach,
-    settings = {
-        python = {
-            analysis = {
-                typeCheckingMode = "off",
-                autoSearchPaths = true,
-                useLibraryCodeForTypes = true,
-            },
-        },
+  on_attach = on_attach,
+  settings = {
+    python = {
+      analysis = {
+        typeCheckingMode = "off",
+        autoSearchPaths = true,
+        useLibraryCodeForTypes = true,
+      },
     },
+  },
+}
+
+lspconfig.ansiblels.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  filetypes = { "yaml", "yml", "yaml.ansible" },
+  cmd = { "ansible-language-server", "--stdio" },
+  settings = {
+    ansible = {
+      ansible = {
+        path = "ansible",
+      },
+      python = {
+        interpreterPath = "python",
+      },
+      validation = {
+        enabled = true,
+        lint = {
+          enabled = true,
+          path = "ansible-lint",
+        },
+      },
+    },
+  },
 }
--- lua/options.lua
+++ lua/options.lua
@@ -11,7 +11,7 @@ vim.wo.number = true
 vim.wo.relativenumber = true
 
 -- Enable mouse mode
-vim.o.mouse = 'a'
+vim.o.mouse = "a"
 --
 -- Set the indentation to 4 spaces
 vim.o.tabstop = 4
@@ -24,13 +24,13 @@ vim.g.loaded_netrwPlugin = 1
 vim.opt.termguicolors = true
 
 -- Treesitter fold related stuff
-vim.o.foldmethod = 'expr'
-vim.o.foldexpr = 'nvim_treesitter#foldexpr()'
+vim.o.foldmethod = "expr"
+vim.o.foldexpr = "nvim_treesitter#foldexpr()"
 
 -- Sync clipboard between OS and Neovim.
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
-vim.o.clipboard = 'unnamedplus'
+vim.o.clipboard = "unnamedplus"
 vim.o.foldenable = false
 
 -- Enable break indent
@@ -44,14 +44,21 @@ vim.o.ignorecase = true
 vim.o.smartcase = true
 
 -- Keep signcolumn on by default
-vim.wo.signcolumn = 'yes'
+vim.wo.signcolumn = "yes"
 
 -- Decrease update time
 vim.o.updatetime = 250
 vim.o.timeoutlen = 300
 
 -- Set completeopt to have a better completion experience
-vim.o.completeopt = 'menuone,noselect'
+vim.o.completeopt = "menuone,noselect"
 vim.o.termguicolors = true
 
 vim.o.conceallevel = 1
+
+-- Ansible
+vim.g.ansible_unindent_after_newline = 1
+vim.g.ansible_attribute_highlight = 'a'
+vim.g.ansible_name_highlight = 'b'
+vim.g.ansible_extra_keywords_highlight = 1
+
